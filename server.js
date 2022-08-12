1aa435a3d49673845de83572a23c551103e02bb1
Lu The Vy <luthevy2001@gmail.com>
2022-08-12 16:14:22 +0700

    update

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+router.render = (req, res) => {
+  var json = require("raw.json");
+  const fs = require("fs");
+  const arr = [];
+  for (let i = 0; i < 8; i++) {
+    var ran = Math.floor(Math.random() * 80);
+    arr[i] = json[ran];
+  }
+  res.jsonp({
+    body: arr,
+  });
+};
+
 server.use(middlewares);
 server.use(router);
-
 server.listen(port);
